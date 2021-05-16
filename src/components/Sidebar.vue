a1a2eae011cf93578d937d348104e0e17ea554c3
Malik Edwards <malikedwards777@gmail.com>
2021-05-16 16:56:18 -0500

    added page skeleton

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="toolbar">
+        <nav class="nav-wrapper">
+            <div class="toolbar">
+                <div class="header-bar">
+                    <span id="brand">  <router-link to="/"><img src="../assets/default.svg" alt="" srcset=""></router-link> </span>
+                </div>
+            
+                <ul id="nav-mobile" class="right">
+                    <li><a class="waves-effect waves-light btn-small"><i class="material-icons">dehaze</i></a></li>
+                    <li><a class="waves-effect waves-light btn-small"><i class="material-icons">person</i><i class="material-icons">expand_more</i></a></li>
+                </ul>
+            </div>
+        </nav>
+        
+        <ul id="slide-out" class="sidenav ">
+            <li>
+                <div class="user-view">
+                    <div class="background">
+                        <img src="images/office.jpg">
+                    </div>
+                    <a href="#name"><span class="white-text name">Dashboard</span></a>
+                </div>
+            </li>
+            <li>
+                <ul class="collapsible expandable">
+                    <li>
+                        <div class="collapsible-header"><a href=""><i class="material-icons">filter_drama</i>Orders</a></div>
+                    </li>
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>Products</div>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a class="waves-effect waves-light btn-small"><i class="material-icons">person</i><i class="material-icons">expand_more</i></a>All Products</li>
+                                <li><a class="waves-effect waves-light btn-small"><i class="material-icons">person</i><i class="material-icons">expand_more</i></a>Add Product</li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="collapsible-header"><a href=""><i class="material-icons">Add Employee</i>Reports</a></div>
+                    </li>
+                    <li>
+                        <div class="collapsible-header"><a href=""><i class="material-icons">filter_drama</i>Reports</a></div>
+                    </li>
+                    <li>
+                        <div class="collapsible-header"><i class="material-icons">filter_drama</i>Setting</div>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a class="waves-effect waves-light btn-small"><i class="material-icons"></i><i class="material-icons">expand_more</i></a>General Setting</li>
+                                <li><a class="waves-effect waves-light btn-small"><i class="material-icons">person</i><i class="material-icons">expand_more</i></a>Payment Setting</li>
+                                <li><a class="waves-effect waves-light btn-small"><i class="material-icons">person</i><i class="material-icons">expand_more</i></a>Email Setting</li>
+                            </ul>
+                        </div>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <a href="#" data-target="slide-out" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+</template>
---
+++ src/views/AllProducts.vue
@@ -0,0 +1,41 @@
+<template>
+             <div class="section">
+                <div class="">
+                    <h3>Latest Orders</h3>
+                    <a class="waves-effect waves-light btn-small">View All</a>
+                </div>
+                <table class="highlight responsive-table">
+                    <thead>
+                        <tr>
+                            <th>SKU</th>
+                            <th>Image</th>
+                            <th>Name</th>
+                            <th>Category</th>
+                            <th>Inventory</th>
+                            <th>Rating</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1234</td>
+                            <td>Alvin Brown</td>
+                            <td>07 Oct, 2021</td>
+                            <td>$400</td>
+                            <td>Paid</td>
+                            <td>Cash</td>
+                            <td><a href=""><i class="material-icons">remove_red_eye</i></a>  <a href=""><i class="material-icons">edit</i></a></td>
+                        </tr>
+                        <tr>
+                            <td>1234</td>
+                            <td>Alvin Brown</td>
+                            <td>07 Oct, 2021</td>
+                            <td>$400</td>
+                            <td>Paid</td>
+                            <td>Cash</td>
+                            <td><a href=""><i class="material-icons">remove_red_eye</i></a>  <a href=""><i class="material-icons">edit</i></a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>   
+</template>
